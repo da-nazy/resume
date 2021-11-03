578063b682b3896cbf00e85229107873d42e78e2
danazy <danielani660@gmail.com>
2021-11-03 12:31:35 +0100

    recnet update

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,7 +1,23 @@
+import '../style/main.css';
 export default function Main() {
     return (
-        <div>
-           <h1>Main</h1> 
+        <div className="about">
+           <div style={{height:'70%',flexDirection:'row',display:'flex',justifyContent:'space-around'}}>
+               <div style={{border:'1px solid black',width:'60%'}} >
+               <h1>Passionate <br/>React Developer</h1>
+               <p>I am a freelance React developer based in Enugu Nigeria. 
+                   I've worked with startups
+                  and established companies. 
+                  I'm passionate about my work.</p>
+                  <button>Hire me</button>
+                </div>
+                <div style={{border:'1px solid black',width:'30%'}}>
+                    <img  style={{backgroundImage:`url(../Asset/Image/avatar.jpg)`}}/>
+                </div>
+           </div>
+           <div style={{border:'1px solid black',height:'20%'}}>
+               <h1>Bottom </h1>
+           </div>
         </div>
     )
 }
